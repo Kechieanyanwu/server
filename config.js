d29bdc095fc4a22bc46fb676fc47e0db96bbd4c2
Nkechi Anyanwu <kechie,94@gmail.com>
2023-06-28 16:54:14 +0100

    Database connection

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = { //contains all configuration keys for third party libs
+    databaseUrl: "mongodb+srv://thenkechianyanwu:tMGMa2VnaX1N7XVw@fashion-database.nw8v3gn.mongodb.net/",
+}
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const express = require('express')
+const express = require('express') //handles node.js server 
 const app = express()
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose')
+const config = require('./config')
+const mongouri = config.databaseUrl
+mongoose.connect(mongouri);
+
+mongoose.connection.on('connected', () => {
+    console.log("Connected to mongo instance");
+});
+mongoose.connection.on('error', err => {
+    console.error("Error connecting to mongo", err);
+})
+
+//create collection users 
+// insert into collection document uche, password, email, nnorom db.users.insertOne({"name": "Uche", "lastname": "Nnorom", email": "email", "pwd": "password"})
+// db.getCollection('users').findOne({email: "var useremail"})
+//change uche = emmanuel db.getCollection('users').updateOne({name: "Uche"}, {name: "Emmanuel"})
+// delete uche db.getCollection('users').deleteOne({name: "Uche"})
+
 
 // Login
 app.get('/login', (req, res) => {
@@ -9,6 +27,13 @@ app.get('/login', (req, res) => {
   //respond you are logged in 
   //respond create an account
   res.send('hello world')
+})
+
+app.post('/createUser', (req, res) => {
+    //receive user input
+    //send to database
+    //create new user in database
+    //respond you now have an account
 })
 
 app.get('/userinfo', (req, res) => {
